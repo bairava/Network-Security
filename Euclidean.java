4ec5cb59151b3493ea078346dacbb5012a3ab321
bairava <bairavaramanch@gmail.com>
2016-06-19 17:58:11 -0700

    Changes to check GCD only when positive numbers were entered

--- Euclidean.java
+++ Euclidean.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 public class Euclidean {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Please enter first positive integer");
+    System.out.println("Please enter two positive integers");
     int a = sc.nextInt();
-    System.out.println("Please enter second positive integer");
     int b = sc.nextInt();
     Euclidean ec= new Euclidean();
-    int result = ec.findgcd(a, b);
-    System.out.println("The GCD of "+ a + " and " + b + " is " + result);
+    int result = 1;
+    
+    if(a>0 && b>0){
+        result = ec.findgcd(a, b);
+        System.out.println("The GCD of "+ a + " and " + b + " is " + result);
+    }
+    else
+        System.out.println("Please enter only positive numbers!!!");
     }
 //Method to find the GCD of two given numbers
 public int findgcd(int a, int b){
-         if(b == 0) 
-             return a;
         while (b != 0)
         {
         int temp = b;
